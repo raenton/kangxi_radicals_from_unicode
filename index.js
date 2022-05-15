6e188dce6f1aa81bc002635ba43c87ebd0b7476a
raenton <raenton@gmail.com>
2022-05-15 16:39:11 +0100

    Added logs for characters without meanings or with special characters

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-const OPEN_CONTENT_WINDOW = true
-const QUERY_FORMAT = "json"
+const API_BASE = "https://en.wikipedia.org/w/api.php"
 const START_CODE = parseInt(0x2F00.toString(16), 16)
 const END_CODE = parseInt(0x2FD5.toString(16), 16)
+const PROPERTY_MARKER = "|"
+const MEANING_PROPERTY = "meaning"
+const NEWLINE_MARKER = "\n"
+const ROOT = document.getElementById("root")
 
-const rootElement = document.getElementById("root")
-
-for (let currentCode = START_CODE; currentCode <= END_CODE; currentCode++) {
-  const position = currentCode - START_CODE + 1
-  const node = document.createElement("p")
-  node.innerText = position + " " + String.fromCodePoint(currentCode)
-  rootElement.appendChild(node)
-}
-
-const API_BASE = "https://en.wikipedia.org/w/api.php"
-const query = {
-  action: "query",
-  prop: "revisions",
-  titles: "Radical_1",
-  rvslots: "*",
-  rvprop: "content",
-  formatversion: "2",
-  format: QUERY_FORMAT,
-  origin: "*"
-}
-
-function querify(query) {
+function querify(baseUrl, query) {
   const keys = Object.keys(query)
   const keyCount = keys.length
-  let result = "?"
+  let result = `${baseUrl}?`
   for (let keyIndex = 0; keyIndex + 1 <= keyCount; keyIndex++) {
     const isLastKey = keyIndex + 1 === keyCount
     const keyName = keys[keyIndex]
@@ -37,45 +19,93 @@ function querify(query) {
   return result
 }
 
-const queryUrl = API_BASE + querify(query)
-
-const wikiPromise = fetch(queryUrl)
-
-if (OPEN_CONTENT_WINDOW) {
-  switch (query["format"]) {
-    case "html":
-      wikiPromise.then(res => res.text())
-        .then(html => {
-          const win = window.open("", "Wiki content", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=200,top=" + (screen.height - 400) + ",left=" + (screen.width - 840))
-          win.document.body.innerHTML = html
-        })
-      break;
-    case "json":
-      wikiPromise.then(res => res.json())
-        .then(json => {
-          const content = json.query?.pages[0]?.revisions[0]?.slots?.main?.content
-
-          if (content) {
-            const win = window.open("", "Wiki content", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=800,top=" + (screen.height - 400) + ",left=" + (screen.width - 840))
-
-            const modifiedContent = content.replaceAll("\n", "<br />", )
-
-            win.document.body.innerHTML = JSON.stringify(modifiedContent, undefined, 2)
-          } else {
-            console.error("Content was not found")
-          }
-        })
-      break;
-    default:
-      console.error("Format is not supported")
+function fetchRadical(radicalNumber, radicalCharacter) {
+  const title = `Radical_${radicalNumber}`
+  const query = {
+    action: "query",
+    prop: "revisions",
+    titles: title,
+    rvslots: "*",
+    rvprop: "content",
+    formatversion: "2",
+    format: "json",
+    origin: "*"
   }
-} else {
-  switch (query["format"]) {
-    case "json":
-      wikiPromise.then(res => res.json())
-        .then(console.log)
-      break;
-    default:
-      console.error("Format is not supported")
-  }
+  return fetch(querify(API_BASE, query))
+    .then(res => res.json())
+    .then(json => parseRadical(radicalNumber, radicalCharacter, json))
 }
+
+function parseRadical(radicalNumber, radicalCharacter, json) {
+  const missing = json["query"]?.["pages"]?.[0]?.["missing"] == true
+  const content = json["query"]?.["pages"]?.[0]?.["revisions"]?.[0]?.["slots"]?.["main"]?.["content"]
+
+  if (missing || !content) {
+    alert("Content was not found")
+    return;
+  }
+
+  return (textContent => {
+    const keyContent = {
+      "number": radicalNumber,
+      "character": radicalCharacter,
+      "meaning": undefined,
+    }
+
+    if (textContent.indexOf(MEANING_PROPERTY) > -1) {
+      const meaningPropertyMarker = `${PROPERTY_MARKER}${MEANING_PROPERTY}=`
+      const meaningIndex = textContent.indexOf(meaningPropertyMarker)
+      const meaningEnd = textContent.indexOf(PROPERTY_MARKER, meaningIndex + 1)
+      const meaningLine = textContent.substring(meaningIndex, meaningEnd)
+      const meaningLineEnd = meaningLine.indexOf(NEWLINE_MARKER)
+      const meaning =
+        meaningLine
+          .replace(meaningPropertyMarker, "")
+          .replace(NEWLINE_MARKER, "")
+          .substring(0, meaningLineEnd)
+          .trim()
+      keyContent["meaning"] = meaning
+    }
+    return keyContent
+  })(content)
+}
+
+async function fetchRadicals() {
+  const radicalPromises = []
+  for (let currentCode = START_CODE; currentCode <= END_CODE; currentCode++) {
+    const position = currentCode - START_CODE + 1
+    const radicalCharacter = String.fromCodePoint(currentCode);
+    radicalPromises.push(fetchRadical(position, radicalCharacter))
+  }
+  return await Promise.all(radicalPromises)
+}
+
+function isEmptyOrFalsy(aString) {
+  return (!aString || aString.length === 0)
+}
+
+function hasSpecialCharacters(aString) {
+  return aString.match(/[^A-Za-z0-9,/() ]/)
+}
+
+function main() {
+  fetchRadicals().then(radicals => {
+    const meaninglessRadicals = []
+    const radicalsWithSpecialCharacters = []
+    radicals.forEach(radical => {
+      if (isEmptyOrFalsy(radical.meaning)) {
+        meaninglessRadicals.push(radical)
+      }
+
+      if (hasSpecialCharacters(radical.meaning)) {
+        radicalsWithSpecialCharacters.push(radical)
+      }
+    })
+
+    console.log(`Found ${meaninglessRadicals.length} radicals without meanings`, meaninglessRadicals)
+    console.log(`Found ${radicalsWithSpecialCharacters.length} radicals with special characters`, radicalsWithSpecialCharacters)
+  })
+}
+
+main()
+
